3a463830a5d3c4b41483a8c8767f819d29d0913f
Ant0yne <antoine.soliv@gmail.com>
2021-11-15 13:51:03 +0100

    test

--- Scene/Main.gd
+++ Scene/Main.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+onready var Arbres = $Arbres
+onready var ArrierePlan = $"Arriere Plan"
+
 func _input(event):
 	if event.is_action_released("ui_cancel"):
 		get_tree().quit()
+
+func _process(delta):
+	Arbres.position.x -= 0.1
+	Arbres.position.y += 0.01
+	ArrierePlan.position.x -= 0.01
+	ArrierePlan.position.y += 0.001
