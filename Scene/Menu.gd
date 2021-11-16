9a0cb8c5a6031887e44ac909f89cefa2376d6232
Ant0yne <antoine.soliv@gmail.com>
2021-11-16 16:49:13 +0100

    Nuit1Saison1

--- Scene/Menu.gd
+++ Scene/Menu.gd
@@ -1,11 +1,28 @@
 extends Node2D
 
+var jour_nuit
+
+func _ready():
+	jour_nuit = OS.get_datetime(false)
+
+func _process(delta):
+	if jour_nuit["hour"] >= 6 && jour_nuit["hour"] < 18:
+		_jour()
+	if jour_nuit["hour"] >= 18 && jour_nuit["hour"] < 6:
+		_nuit()
+	
+func _jour():
+	get_tree().change_scene("res://Scene/Jour1Saison1.tscn")
+	
+func _nuit():
+	get_tree().change_scene("res://Scene/Nuit1Saison1.tscn")
+
 func _input(event):
 	if event.is_action_released("ui_cancel"):
 		get_tree().quit()
 
 func _on_Commencer_button_up():
-	get_tree().change_scene("res://Scene/Main.tscn")
+	get_tree().change_scene("res://Scene/Jour1Saison1.tscn")
 
 func _on_Quitter_button_up():
 	get_tree().quit()
